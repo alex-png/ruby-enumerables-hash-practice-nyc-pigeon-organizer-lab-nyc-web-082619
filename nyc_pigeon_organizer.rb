35431e74bfbb122d0cc8cf004d23aa9eca8961c6
Alex Rodriguez <alexrod102797@gmail.com>
2019-08-19 18:56:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new = {}
+  attributes = {}
+  elements = []
+  data.keys.each do |out_key|
+      if attributes[out_key]
+        else
+        attributes[out_key] = []
+        end
+    data[out_key].keys.each do |inner_key|
+      data[out_key][inner_key].each do |name|
+      if new[name]
+        else
+        new[name] = attributes
+        end
+      end      
+    end
+  end
+return new
 end
