07cd0d2902325bf4eb86dd27431750400bf8bc02
Alex Rodriguez <alexrod102797@gmail.com>
2019-08-19 21:41:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,39 @@
 def nyc_pigeon_organizer(data)
   new = {}
   attributes = {}
-  elements = []
+  name_array = []
   data.keys.each do |out_key|
-      if attributes[out_key]
-        else
-        attributes[out_key] = []
+    data[out_key].keys.each do |in_key|
+      data[out_key][in_key].each do |name|
+        if name_array.include?(name)
+        else 
+          name_array << name
         end
-    data[out_key].keys.each do |inner_key|
-      data[out_key][inner_key].each do |name|
-      if new[name]
-        else
-        new[name] = attributes
-        end
-      end      
+      end
     end
   end
-return new
+  name_array.each do |names|
+    new[names] = {:color => [], :gender => [], :lives => []}
+  end
+  name_array.each do |name|
+    data[:color].keys.each do |colors|
+      if data[:color][colors].include?(name) && new[name][:color].none?(colors)
+      new[name][:color] << colors.to_s
+      end
+    end
+    data[:gender].keys.each do |genders|
+      if data[:gender][genders].include?(name) && new[name][:gender].none?(genders)
+      new[name][:gender] << genders.to_s
+      end
+    end
+    data[:lives].keys.each do |loc|
+      if data[:lives][loc].include?(name) && new[name][:lives].none?(loc)
+       new[name][:lives] << loc.to_s
+      end
+    end
+  
+  end
+puts new
+new
 end
+nyc_pigeon_organizer(pigeon_data)
